6d7095cf39ed03d558fc5e01cb25755fcf4c5010
Casper Olsen <gemic@live.dk>
2023-04-16 16:46:25 +0200

    Added command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser)]
+struct Args {
+    /// Token to use
+    #[arg(short, long)]
+    token: String,
+
+    ///
+    #[arg(long, value_enum, default_value_t = TokenType::Jwt)]
+    token_type: TokenType,
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum TokenType {
+    Jwt,
+}
+
 fn main() {
-    
+    let args = Args::parse();
+
+    println!("Hello {}!", args.token);
 }
